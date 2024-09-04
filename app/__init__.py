09f5c511264cc8843a73cc2ae1e4cfeca5e78279
codefher-segip <codefher@gmail.com>
2024-09-04 16:34:18 -0400

    clean

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,10 @@
 # app/__init__.py
 from flask import Flask
 from .routes import configure_routes
-# asegúrate de importar cualquier otra configuración o inicialización necesaria aquí
 
 def create_app():
     app = Flask(__name__)
-    # Configuraciones adicionales, como app.config['DEBUG'] = True, etc.
     
-    configure_routes(app)  # Asegúrate de que esta función esté definida en routes.py
+    configure_routes(app)  
 
     return app
